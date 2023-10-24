86c415d41da9584b81fe10f372a334fcd73753fa
I_am_Rohit9827 <116993691+some-coder-whowantstocode@users.noreply.github.com>
2023-10-24 23:34:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const cors = require('cors')
 
 const corsOptions ={
-  origin: 'http://localhost:5173',
+  origin: 'process.env.url',
   credentials: true,
   optionSuccessStatus: 200
 }
